66bd4bce1224fd8c711783fe3d13ad907367deff
krishpranav <krisna.pranav@gmail.com>
2021-05-16 12:27:06 +0530

    status

---
+++ models/status.rb
@@ -0,0 +1,12 @@
+module Twitterwatch
+    module Models
+      class Status < Sequel::Model
+        many_to_one :workspace
+        many_to_one :user
+        many_to_many :mentions
+        many_to_many :hashtags
+        many_to_many :urls
+      end
+    end
+  end
+  
