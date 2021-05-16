5b73c69cc1b824226f391e51d42df4d2b46da393
krishpranav <krisna.pranav@gmail.com>
2021-05-16 16:12:59 +0530

    run func

---
+++ lib/twitterwatch/commands/run.rb
@@ -0,0 +1,20 @@
+module Twitterwatch
+    module Commands
+      class Run < Twitterwatch::Command
+        self.meta = {
+          :description => "Run current module",
+          :names       => %w(run execute),
+          :usage       => "run"
+        }
+  
+        def run
+          if !console.current_module
+            error("No module loaded")
+            return false
+          end
+          console.current_module.new.execute
+        end
+      end
+    end
+  end
+  
